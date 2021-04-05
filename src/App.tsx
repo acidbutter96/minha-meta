a8c045c5bc8d0bfe3fd88792a8ce0dc9ea7d2645
acidbutter96 <marknotas@outlook.com>
2021-04-05 00:28:29 -0300

    build: prepare environment and install react with typescript template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App: React.FC = () =>{
+  return (
+    <>
+      <h1>Monthly Sales Chart</h1>
+    </>
+  );
+}
+
+export default App;
